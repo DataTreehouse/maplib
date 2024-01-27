b00b48096641f53d3d0686127646591caa8acab1
Magnus Bakken <10287813+magbak@users.noreply.github.com>
2024-01-27 10:08:34 +0100

    Test for new pvalues construction

--- py_maplib/tests/test_pizza_example.py
+++ py_maplib/tests/test_pizza_example.py
@@ -57,3 +57,48 @@ def test_simple_query_no_error(pizzas_mapping):
 
     expected_df = pl.DataFrame({"p": ["<https://github.com/magbak/maplib/pizza#Hawaiian>"]})
     assert_frame_equal(res, expected_df)
+
+
+def test_construct_pvalues(pizzas_mapping):
+    h_df = pl.DataFrame({"h1": ["<https://github.com/magbak/maplib/pizza#Hawaiian1>", "<https://github.com/magbak/maplib/pizza#Hawaiian2>"],
+                         "h2": ["<https://github.com/magbak/maplib/pizza#Hawaiian3>", "<https://github.com/magbak/maplib/pizza#Hawaiian4>"]})
+
+    res = pizzas_mapping.query("""
+    PREFIX pizza:<https://github.com/magbak/maplib/pizza#>
+
+    CONSTRUCT {
+        ?h1 a pizza:Pizza . 
+        ?h2 a pizza:Pizza .
+    } WHERE {
+        PVALUES (?h1 ?h2) h
+    }
+    """, parameters={"h":h_df})
+    res0 = res[0]
+    expected_dtypes = {'object': 'IRI', 'subject': 'IRI'}
+    assert res0.rdf_datatypes == expected_dtypes
+    res1 = res[1]
+    assert res1.rdf_datatypes == expected_dtypes
+    assert res0.height == 2
+    assert res1.height == 2
+
+
+def test_construct_pvalues2(pizzas_mapping):
+    h_df = pl.DataFrame({"h1": ["<https://github.com/magbak/maplib/pizza#Hawaiian>", "<https://github.com/magbak/maplib/pizza#Hawaiian2>"]})
+    res = pizzas_mapping.query("""
+    PREFIX pizza:<https://github.com/magbak/maplib/pizza#>
+
+    CONSTRUCT {
+        ?h1 a pizza:Pizza . 
+        ?i a pizza:Pizza .
+    } WHERE {
+        ?h1 pizza:hasIngredient ?i .
+        PVALUES (?h1) h
+    }
+    """, parameters={"h":h_df})
+    res0 = res[0]
+    expected_dtypes = {'object': 'IRI', 'subject': 'IRI'}
+    assert res0.rdf_datatypes == expected_dtypes
+    res1 = res[1]
+    assert res1.rdf_datatypes == expected_dtypes
+    assert res0.height == 1
+    assert res1.height == 2
