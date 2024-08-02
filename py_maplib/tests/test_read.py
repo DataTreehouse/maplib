23822741d2f69943c338cf932fed4d8af51c724a
Magnus Bakken <10287813+magbak@users.noreply.github.com>
2024-08-02 17:58:37 +0200

    Fix deps, add test, bump version

--- py_maplib/tests/test_read.py
+++ py_maplib/tests/test_read.py
@@ -27,6 +27,23 @@ def test_read_ntriples():
     expected_df = pl.scan_csv(filename).collect()
     pl.testing.assert_frame_equal(res, expected_df)
 
+def test_read_ntriples_twice_with_replace():
+    m = Mapping()
+    m.read_triples(str(TESTDATA_PATH / "read_ntriples.nt"))
+    m.read_triples(str(TESTDATA_PATH / "read_ntriples.nt"), replace_graph=True)
+    res = m.query("""
+            PREFIX foaf:<http://xmlns.com/foaf/0.1/>
+
+            SELECT ?s ?v ?o WHERE {
+            ?s ?v ?o .
+            } ORDER BY ?s ?v ?o
+            """).sort(["s", "v", "o"])
+    # TODO: Fix multitype sorting
+    filename = TESTDATA_PATH / "read_ntriples.csv"
+    #res.write_csv(str(filename))
+    assert res.shape == (8,3)
+
+
 def test_read_write_ntriples_string():
     m = Mapping()
     with open(TESTDATA_PATH / "read_ntriples.nt") as f:
