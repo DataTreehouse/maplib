71f2bc085c9d2e491e3d84b660d2c49dba7ef1e3
Magnus Bakken <10287813+magbak@users.noreply.github.com>
2024-12-27 11:27:05 +0100

    Fix api stub

--- lib/report_mapping/src/lib.rs
+++ lib/report_mapping/src/lib.rs
@@ -1,10 +1,11 @@
 use maplib::mapping::Mapping;
 use shacl::ValidationReport;
-use triplestore::Triplestore;
+use triplestore::{IndexingOptions, Triplestore};
 
 pub fn report_to_mapping(
     _report: &ValidationReport,
     _shape_graph: &Option<Triplestore>,
+    _indexing: Option<IndexingOptions>
 ) -> Mapping {
     unimplemented!("Contact Data Treehouse to try")
 }
