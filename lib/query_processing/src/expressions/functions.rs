e8f85235fe35817a0d10444d1907e05bdee6615f
Magnus Bakken <10287813+magbak@users.noreply.github.com>
2026-04-06 21:37:36 +0200

    Bugfix for rounding

--- lib/query_processing/src/expressions/functions.rs
+++ lib/query_processing/src/expressions/functions.rs
@@ -16,13 +16,12 @@ use rayon::iter::IntoParallelIterator;
 use rayon::iter::ParallelIterator;
 use representation::cats::{maybe_decode_expr, LockedCats};
 use representation::multitype::{MULTI_BLANK_DT, MULTI_IRI_DT};
-use representation::polars_to_rdf::df_as_result;
 use representation::query_context::Context;
 use representation::rdf_to_polars::{
-    polars_literal_values_to_series, rdf_literal_to_polars_literal_value,
+    polars_literal_values_to_series,
     rdf_literal_to_polars_literal_value_impl, rdf_named_node_to_polars_literal_value,
 };
-use representation::solution_mapping::{BaseCatState, EagerSolutionMappings, SolutionMappings};
+use representation::solution_mapping::{BaseCatState, SolutionMappings};
 use representation::{
     BaseRDFNodeType, RDFNodeState, LANG_STRING_LANG_FIELD, LANG_STRING_VALUE_FIELD,
 };
@@ -296,9 +295,53 @@ pub fn func_expression(
                 .rdf_node_types
                 .get(first_context.as_str())
                 .unwrap();
-            solution_mappings
-                .rdf_node_types
-                .insert(outer_context.as_str().to_string(), existing_type.clone());
+            let mut rounded_exprs = vec![];
+            let mut rounded_types = vec![];
+            if existing_type.is_multi() {
+                for t in existing_type.map.keys() {
+                    if t.is_numeric() {
+                        rounded_exprs.push(col(first_context.as_str()).struct_().field_by_name(&t.field_col_name())
+                            .round(0, RoundMode::HalfAwayFromZero).alias(&t.field_col_name()));
+                        rounded_types.push(t.clone());
+                    }
+                }
+            } else if existing_type.is_numeric() {
+                rounded_exprs.push(col(first_context.as_str())
+                    .round(0, RoundMode::HalfAwayFromZero));
+                rounded_types.push(existing_type.get_base_type().unwrap().clone());
+            }
+            if rounded_exprs.is_empty() {
+                solution_mappings.mappings = solution_mappings.mappings.with_column(
+                    lit(LiteralValue::untyped_null())
+                        .cast(BaseRDFNodeType::None.default_input_polars_data_type())
+                        .alias(outer_context.as_str()),
+                );
+                solution_mappings.rdf_node_types.insert(
+                    outer_context.as_str().to_string(),
+                    BaseRDFNodeType::None.into_default_input_rdf_node_state(),
+                );
+            } else if rounded_exprs.len() == 1 {
+                solution_mappings.mappings = solution_mappings.mappings.with_column(
+                    rounded_exprs.pop().unwrap().alias(outer_context.as_str()),
+                );
+                solution_mappings.rdf_node_types.insert(
+                    outer_context.as_str().to_string(),
+                    rounded_types.pop().unwrap().into_default_stored_rdf_node_state()
+                );
+            } else {
+                solution_mappings.mappings = solution_mappings.mappings.with_column(
+                    as_struct(rounded_exprs).alias(outer_context.as_str()),
+                );
+                let mut types_map = HashMap::new();
+                for t in rounded_types {
+                    let s = t.default_input_cat_state();
+                    types_map.insert(t, s);
+                }
+                solution_mappings.rdf_node_types.insert(
+                    outer_context.as_str().to_string(),
+                    RDFNodeState::from_map(types_map)
+                );
+            }
         }
         Function::Str => {
             if args.len() != 1 {
