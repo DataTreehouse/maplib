77fdf663ac20ada7efe0d2f97b36618be96ca00f
vennyy3 <vendelasoiland@hotmail.com>
2026-06-05 15:25:43 +0200

    refactor: moved StrDt function

--- lib/query_processing/src/expressions/functions.rs
+++ lib/query_processing/src/expressions/functions.rs
@@ -9,6 +9,7 @@ mod sparql_regex;
 mod sparql_uuid;
 mod str_;
 mod struuid;
+mod str_dt;
 
 use crate::constants::{
     DATETIME_AS_MICROS, DATETIME_AS_SECONDS, DECODE, FLOOR_DATETIME_TO_SECONDS_INTERVAL,
@@ -39,10 +40,7 @@ use polars::prelude::{
 use representation::cats::{maybe_decode_expr, LockedCats};
 use representation::multitype::{MULTI_BLANK_DT, MULTI_IRI_DT};
 use representation::query_context::Context;
-use representation::rdf_to_polars::{
-    polars_literal_values_to_series, rdf_literal_to_polars_literal_value_impl,
-    rdf_named_node_to_polars_literal_value,
-};
+use representation::rdf_to_polars::rdf_named_node_to_polars_literal_value;
 use representation::solution_mapping::{BaseCatState, SolutionMappings};
 use representation::{
     BaseRDFNodeType, RDFNodeState, LANG_STRING_LANG_FIELD, LANG_STRING_VALUE_FIELD,
@@ -52,6 +50,7 @@ use spargebra::algebra::{Expression, Function};
 use std::collections::HashMap;
 use std::ops::{Div, Mul};
 use uri_encode::encode_uri;
+use crate::expressions::functions::str_dt::str_dt;
 
 pub fn func_expression(
     mut solution_mappings: SolutionMappings,
@@ -565,102 +564,7 @@ pub fn func_expression(
             }
         }
         Function::StrDt => {
-            if args.len() != 2 {
-                return Err(QueryProcessingError::BadNumberOfFunctionArguments(
-                    func.clone(),
-                    args.len(),
-                    "2".to_string(),
-                ));
-            }
-            let first_context = args_contexts.get(&0).unwrap();
-            let first_type = solution_mappings
-                .rdf_node_types
-                .get(first_context.as_str())
-                .unwrap();
-            let base_string = BaseRDFNodeType::Literal(xsd::STRING.into_owned());
-            let use_base = if first_type.is_lit_type(xsd::STRING) {
-                Some((
-                    first_type.get_base_state().unwrap(),
-                    col(first_context.as_str()),
-                ))
-            } else if first_type.is_multi() {
-                if let Some(base_state) = first_type.map.get(&base_string) {
-                    Some((
-                        base_state,
-                        col(first_context.as_str())
-                            .struct_()
-                            .field_by_name(&base_string.field_col_name()),
-                    ))
-                } else {
-                    None
-                }
-            } else {
-                None
-            };
-            if let Some((use_base_state, use_base_col)) = use_base {
-                let dt_arg = args.get(1).unwrap();
-                let dt_iri = if let Expression::NamedNode(nn) = &dt_arg {
-                    nn.clone()
-                } else {
-                    return Err(QueryProcessingError::BadArgument(format!(
-                        "STRDT second argument should be IRI"
-                    )));
-                };
-                let out_dt = BaseRDFNodeType::Literal(dt_iri.clone());
-                let out_polars_type = out_dt.default_input_polars_data_type();
-                let outer_context_cloned = outer_context.clone();
-                let decoded_col =
-                    maybe_decode_expr(use_base_col, &base_string, use_base_state, global_cats);
-                let expr = decoded_col.map(
-                    move |x| {
-                        let litval: Vec<_> = x
-                            .str()?
-                            .iter()
-                            .map(|x| {
-                                if let Some(s) = x {
-                                    rdf_literal_to_polars_literal_value_impl(
-                                        s,
-                                        dt_iri.clone().as_ref(),
-                                    )
-                                } else {
-                                    LiteralValue::untyped_null()
-                                }
-                            })
-                            .collect();
-                        let series = polars_literal_values_to_series(
-                            litval,
-                            outer_context_cloned.clone().as_str(),
-                        );
-                        Ok(series.into_column())
-                    },
-                    move |_, f| {
-                        let mut field = f.clone();
-                        field.dtype = out_polars_type.clone();
-                        Ok(field)
-                    },
-                );
-                solution_mappings.mappings = solution_mappings
-                    .mappings
-                    .with_column(expr.alias(outer_context.as_str()));
-                solution_mappings.rdf_node_types.insert(
-                    outer_context.as_str().to_string(),
-                    out_dt.into_default_input_rdf_node_state(),
-                );
-            } else {
-                solution_mappings.mappings = solution_mappings.mappings.with_column(
-                    lit(LiteralValue::untyped_null())
-                        .cast(
-                            BaseRDFNodeType::None
-                                .into_default_input_rdf_node_state()
-                                .polars_data_type(),
-                        )
-                        .alias(outer_context.as_str()),
-                );
-                solution_mappings.rdf_node_types.insert(
-                    outer_context.as_str().to_string(),
-                    BaseRDFNodeType::None.into_default_input_rdf_node_state(),
-                );
-            }
+            solution_mappings = str_dt(solution_mappings, func, args, &args_contexts, outer_context, global_cats)?;
         }
         Function::StrBefore | Function::StrAfter => {
             if (args.len() != 2) {
