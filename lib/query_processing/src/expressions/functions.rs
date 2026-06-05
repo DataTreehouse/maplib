eac088eecbba0ae09f737fc641f230e20030256f
vennyy3 <vendelasoiland@hotmail.com>
2026-06-05 11:57:16 +0200

    refactor: moved struuid

--- lib/query_processing/src/expressions/functions.rs
+++ lib/query_processing/src/expressions/functions.rs
@@ -1,8 +1,12 @@
+mod struuid;
+mod replace;
+
 use crate::constants::{
     DATETIME_AS_MICROS, DATETIME_AS_SECONDS, DECODE, FLOOR_DATETIME_TO_SECONDS_INTERVAL,
     MICROS_AS_DATETIME, MODULUS, SECONDS_AS_DATETIME,
 };
 use crate::errors::QueryProcessingError;
+use crate::expressions::functions::struuid::struuid;
 use crate::expressions::{cast_lang_string_to_string, drop_inner_contexts};
 use md5::{Digest, Md5};
 use oxrdf::vocab::{rdf, xsd};
@@ -760,41 +764,7 @@ pub fn func_expression(
             );
         }
         Function::StrUuid => {
-            if !args.is_empty() {
-                return Err(QueryProcessingError::BadNumberOfFunctionArguments(
-                    func.clone(),
-                    args.len(),
-                    "0".to_string(),
-                ));
-            }
-            let tmp_column = uuid::Uuid::new_v4().to_string();
-            solution_mappings.mappings = solution_mappings
-                .mappings
-                .with_row_index(PlSmallStr::from_str(&tmp_column), None);
-            solution_mappings.mappings = solution_mappings.mappings.with_column(
-                col(&tmp_column)
-                    .map(
-                        |c| {
-                            let uuids: Vec<_> = (0..c.len())
-                                .into_par_iter()
-                                .map(|_| uuid::Uuid::new_v4().to_string())
-                                .collect();
-                            let s = Series::new("uuids".into(), uuids);
-                            Ok(s.into_column())
-                        },
-                        |_, f| Ok(Field::new(f.name().clone(), DataType::String)),
-                    )
-                    .alias(outer_context.as_str()),
-            );
-            solution_mappings.mappings =
-                solution_mappings
-                    .mappings
-                    .drop(by_name([&tmp_column], true, false));
-            solution_mappings.rdf_node_types.insert(
-                outer_context.as_str().to_string(),
-                BaseRDFNodeType::Literal(xsd::STRING.into_owned())
-                    .into_default_input_rdf_node_state(),
-            );
+            solution_mappings = struuid(solution_mappings, func, args, outer_context)?;
         }
         Function::Replace => {
             if args.len() != 3 && args.len() != 4 {
